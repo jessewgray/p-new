ff8f1121b59616f5fe82e82cb256ddfdab0274ef
jessewgray <jwgray2@gmail.com>
2023-10-11 10:01:25 -0400

    updated ci env

--- src/DisplayInfo.js
+++ src/DisplayInfo.js
@@ -54,7 +54,7 @@ function DisplayInfo(){
                 return () => clearTimeout(timer);
               }, []);
               
-              console.log('tesing oct 11')
+              console.log('tesing oct 11 try 2')
         
         return(
             <div className='container'>                
