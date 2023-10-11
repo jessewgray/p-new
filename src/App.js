5c687cdb918b8cb32cf4ca8ca01ec2aa56d284fb
jessewgray <jwgray2@gmail.com>
2023-10-11 10:15:57 -0400

    adding trail slash to pages

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+              <Route path="/login/" element={<Login />} />
+              <Route path="/register/" element={<Register />} />
             </Routes>
           </BrowserRouter>
         </div>
